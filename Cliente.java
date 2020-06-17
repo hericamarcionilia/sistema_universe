f0fb18262f3dcdcdb608a06e4ea51755f1b2d7f2
hericamarcionilia <67062500+hericamarcionilia@users.noreply.github.com>
2020-06-17 12:14:24 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Herica Marcionilia
+ */
+public class Cliente {
+    public String produto;
+    public String preco;
+    public String quantidade;
+    public String id;
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+}
